3f6168f587f5c75c9dc92ee74d5149417f55ef70
Marwan <marwan.benrhannou@gmail.com>
2021-08-25 20:12:02 +0200

    Render Triangle with Opengl Pipeline

--- QuestEngine/main.cpp
+++ QuestEngine/main.cpp
@@ -2,29 +2,22 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-// Fr : Squelette de l'initialisation d'une fenêtre Opengl avec GLFW et glad
-// En : Skeleton of the initialization of Opengl window with GLFW and Glad
+
 int main()
 {
-	// Fr : GLFW initialisation
-	// En : GLFW initialization
 	int result = glfwInit();
 	if (result == GLFW_TRUE)
-		std::cout << "GLFW is initialised" << std::endl;
+		std::cout << "GLFW is initialised" << std::endl; 
 	else
-		std::cout << "GLFW is not initialised" << std::endl;
+		std::cout << "GLFW is not initialised" << std::endl; 
 
-	// Fr/En : Opengl Context configuration
 	glfwInitHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwInitHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 	glfwInitHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Fr : Création de la fenêtre
-	// En : Window creation
 	int width = 1280;
 	int height = 720;
 	const char* title = "First Opengl Window";
-	
 	GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
 	if (!window)
 	{
@@ -33,12 +26,8 @@ int main()
 		return -1;
 	}
 
-	// Fr : Lié le contexte opengl à la fenêtre GLFW
-	// En : Link opengl context to GLFW window
 	glfwMakeContextCurrent(window);
 		
-	// Fr : Chargement des fonctions Opengl
-	// En : Opengl function loading
 	result = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 	if (!result)
 	{
@@ -47,13 +36,91 @@ int main()
 		return -1;
 	}
 
-	// Fr : boucle de rendu
-	// En : rendering loop
+	float vertices[] = { -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f };
+
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	
+	//Vertex Shader Configuration
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+
+	int  success = 0;
+	char infoLog[512];
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "Vertex Sahder Compilation Failed\n" << infoLog << std::endl;
+	}
+
+	//Fragement Shader Configuration
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FdfragColor;\n"
+		"void main()\n"
+		"{\n"
+		"	FdfragColor = vec4(0.0f, 0.0f, 1.0f, 1.0f)\n;"
+		"}\0";
+
+
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "Fragment Shader Compilation Failed\n" << infoLog << std::endl;
+	}
+
+
+	//Shader Program Configuration
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "Shader Program link Compilation Failed\n" << infoLog << std::endl;
+	}
+
+	glUseProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	
+	// Game Engine Loop
 	while (!glfwWindowShouldClose(window))
 	{
-		glClearColor(1.0f, 1.0f, 1.0f,1.0f);
+		glClearColor(0.6f, 0.6f, 0.6f,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);	
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 }
+
